da0d69ec3eb7674f7ec9ed4af04c6e07172e50e0
ichang14 <cha24056@byui.edu>
2026-05-27 12:59:14 -0400

    fix rubric

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,31 @@ year.textContent = new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent =
 `Last Modification: ${document.lastModified}`;
+
+
+// WIND CHILL
+
+const temp = 45;
+const speed = 10;
+
+function calculateWindChill(temp, speed) {
+    return (
+        35.74 +
+        (0.6215 * temp) -
+        (35.75 * Math.pow(speed, 0.16)) +
+        (0.4275 * temp * Math.pow(speed, 0.16))
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temp <= 50 && speed > 3) {
+
+    windChill.textContent =
+        `${calculateWindChill(temp, speed)} °F`;
+
+} else {
+
+    windChill.textContent = "N/A";
+
+}
